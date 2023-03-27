93232e0d36d569cb92218a959dfbcb1c01a52752
Arthur Lopes <arthurlopes558@gmail.com>
2023-03-27 20:54:41 -0300

    otimizado

--- script.js
+++ script.js
@@ -9,52 +9,28 @@ let propiedadesElemento = document.querySelector('.propiedades');
 let btnCopiar =  document.querySelector('#btn-copiar');
 
 const valoresBorda = {
-    'borderTopL': 0,
-    'borderTopR': 0,
-    'borderBottomL': 0,
-    'borderBottomR': 0
+    'borderTopL': `0px`,
+    'borderTopR': `0px`,
+    'borderBottomL': `0px`,
+    'borderBottomR': `0px`
 }
 
-propiedadesElemento.innerHTML = `border-radius: ${valoresBorda.borderTopL}px,${valoresBorda.borderTopR}px,${valoresBorda.borderBottomL}px,${valoresBorda.borderBottomR}px`
+const aoAlterar = (input, propsBorder, borderSide) => {
+    //altera o radius 
+    let valorDoMix = input.value;
+    elemento.style[propsBorder] = `${valorDoMix}px`;
+    
 
-const aoAlterarTopLeft = () => {
-    let valorTopLeft = mixTopLeft.value;
-    elemento.style.borderTopLeftRadius = `${valorTopLeft}px`;
-    valoresBorda.borderTopL = elemento.style.borderTopLeftRadius;
-
-    propiedadesElemento.innerHTML = `border-radius: ${valoresBorda.borderTopL},${valoresBorda.borderTopR},${valoresBorda.borderBottomL},${valoresBorda.borderBottomR}`
+    //atualiza as propiedades
+    valoresBorda[borderSide] = elemento.style[propsBorder];
+    propiedadesElemento.innerHTML = `border-radius: ${valoresBorda.borderTopL}, ${valoresBorda.borderTopR}, ${valoresBorda.borderBottomL}, ${valoresBorda.borderBottomR}`
 }
 
-const aoAlterarTopRight = () => {
-    let valorTopRight = mixTopRight.value;
-    elemento.style.borderTopRightRadius = `${valorTopRight}px`
-    valoresBorda.borderTopR = elemento.style.borderTopRightRadius;
 
-    propiedadesElemento.innerHTML = `border-radius: ${valoresBorda.borderTopL},${valoresBorda.borderTopR},${valoresBorda.borderBottomL},${valoresBorda.borderBottomR}`
-}
-
-const aoAlterarBottomLeft = () => {
-    let valorBottomLeft = mixBottomLeft.value;
-    elemento.style.borderBottomLeftRadius = `${valorBottomLeft}px`
-
-    valoresBorda.borderBottomL = elemento.style.borderBottomLeftRadius;
-
-    propiedadesElemento.innerHTML = `border-radius: ${valoresBorda.borderTopL},${valoresBorda.borderTopR},${valoresBorda.borderBottomL},${valoresBorda.borderBottomR}`
-
-}
-const aoAlterarBottomRight = () => {
-    let valorBottomRight = mixBottomRight.value;
-    elemento.style.borderBottomRightRadius = `${valorBottomRight}px`
-
-    valoresBorda.borderBottomR = elemento.style.borderBottomRightRadius;
-
-    propiedadesElemento.innerHTML = `border-radius: ${valoresBorda.borderTopL},${valoresBorda.borderTopR},${valoresBorda.borderBottomL},${valoresBorda.borderBottomR}`
-}
-
-mixTopLeft.addEventListener('input', aoAlterarTopLeft)
-mixTopRight.addEventListener('input', aoAlterarTopRight);
-mixBottomLeft.addEventListener('input', aoAlterarBottomLeft);
-mixBottomRight.addEventListener('input', aoAlterarBottomRight);
+mixTopLeft.addEventListener('input', () => aoAlterar(mixTopLeft,'borderTopLeftRadius','borderTopL'))
+mixTopRight.addEventListener('input', () => aoAlterar(mixTopRight,'borderTopRightRadius', 'borderTopR'));
+mixBottomLeft.addEventListener('input', () => aoAlterar(mixBottomLeft,'borderBottomLeftRadius','borderBottomL'));
+mixBottomRight.addEventListener('input',() => aoAlterar(mixBottomRight,'borderBottomRightRadius', 'borderBottomR'));
 
 const corRadius = () => {
     let cor = colorRadius.value;
@@ -72,5 +48,6 @@ colorBorder.addEventListener('input', corBorder)
 
 btnCopiar.addEventListener('click', function(){
   navigator.clipboard.writeText(propiedadesElemento.innerHTML)
+
 })
 
